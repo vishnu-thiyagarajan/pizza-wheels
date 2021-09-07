30e63b3df76829af9f41693149d8e667a35b5bfa
Vishnu Thiyagarajan <Monocept@MONO-HYD-LAP136.corp.monocept.com>
2021-09-07 16:08:01 +0530

    connecting db check 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 import { Provider } from "react-redux";
+import axios from "axios";
 
 import store from "../redux/store";
 import PublicRoutes from "./routes/PublicRoutes";
 import PrivateRoutes from "./routes/PrivateRoutes";
 import ProtectedRoute from "./routes/ProtectedRoute";
+
+axios.defaults.baseURL = process.env.REACT_APP_DBURL;
+
 function App() {
+  axios
+    .get("/")
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   return (
     <>
       <Provider store={store}>
