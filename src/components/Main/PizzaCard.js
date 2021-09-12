8ddf990b182f7b53c912838c09951d536158b0ff
Vishnu Thiyagarajan <Monocept@MONO-HYD-LAP136.corp.monocept.com>
2021-09-12 22:28:24 +0530

    fix unused vars

--- src/components/Main/PizzaCard.js
+++ src/components/Main/PizzaCard.js
@@ -7,7 +7,7 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 const useStyles = makeStyles({
   media: {
